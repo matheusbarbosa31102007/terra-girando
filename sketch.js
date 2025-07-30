7104f47af17b16de55082ba088e7334a1704e960
MATHEUS BARBOSA <141179201+matheusbarbosa31102007@users.noreply.github.com>
2025-07-30 13:51:36 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ function setup() {
 }
 
 function draw() {
-  background(205, 102, 94); //cor de fundo
+  background(205, 182, 45); //cor de fundo
   rotateY(millis()/2000); // rotação da terra
   texture(terra); //textura da terra, imagem.
   sphere(300, 450); //esfera 3D
